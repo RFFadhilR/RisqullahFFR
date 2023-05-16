0cdde9dcdce724995f6aa2dc8c02fec4fd277c8e
RFFadhilR <47029808+RFFadhilR@users.noreply.github.com>
2023-05-16 07:07:41 +0000

    Modul Praktikum 1: Membuat Aplikasi Web dengan Python Dash

--- main.py
+++ main.py
@@ -1,10 +1,88 @@
-from flask import *
+# PRAKTIKUM 1
 
-app = Flask(__name__)
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
 
-@app.route("/")
-def hello_world():
-  return render_template('index.html')
+# # Membuat instance Dash
+# app = dash.Dash(__name__)
 
+# # Layout aplikasi
+# app.layout = html.Div(children=[
+#   html.H1("Selamat Datang di Aplikasi Web Sederhana"),
+#   dcc.Input(id="input-text", type="text", placeholder="Masukkan teks"),
+#   html.Button("Kirim", id="submit-button", n_clicks=0),
+#   html.Div(id="output")
+# ])
+
+# # Callback untuk merespon input pengguna
+# @app.callback(dash.dependencies.Output("output", "children"),
+#               [dash.dependencies.Input("submit-button", "n_clicks")],
+#               [dash.dependencies.State("input-text", "value")])
+# def update_output(n_clicks, input_text):
+#   if n_clicks > 0 and input_text:
+#     return html.H2(f"Anda mengirim: {input_text}")
+#   else:
+#     return ""
+
+# #menjalankan aplikasi
+# if __name__ == '__main__':
+#   app.run_server(debug=True)
+
+# Membuat instance Dash
+app = dash.Dash(__name__)
+
+# Layout aplikasi
+app.layout = html.Div(children=[
+  html.H1("Aplikasi Catatan Harian"),
+  html.Div(children=[
+    dcc.Textarea(id="note-input",
+                 placeholder="Masukkan catatan di sini",
+                 style={
+                   "width": "100%",
+                   "height": "100px"
+                 }),
+    html.Button("Simpan", id="save-button", n_clicks=0),
+  ],
+           style={"margin-bottom": "20px"}),
+  html.Div(id="message"),
+  html.H2("Daftar Catatan Harian"),
+  html.Ul(id="note-list"),
+],
+                      style={
+                        "max-width": "600px",
+                        "margin": "auto"
+                      })
+
+
+# Callback untuk menyimpan catatan harian
+@app.callback(Output("message",
+                     "children"), [Input("save-button", "n_clicks")],
+              [State("note-input", "value")])
+def save_note(n_clicks, note):
+  if n_clicks > 0 and note:
+    # Simpan catatan ke basis data atau tempat penyimpanan lainnya
+    # Di sini, hanya mencetak catatan ke konsol sebagai contoh
+    print("Catatan baru:", note)
+    return html.P("Catatan berhasil disimpan!")
+  else:
+    return ""
+
+
+# Callback untuk menampilkan daftar catatan harian
+@app.callback(Output("note-list", "children"),
+              [Input("save-button", "n_clicks")])
+def display_notes(n_clicks):
+  if n_clicks > 0:
+    # Ambil daftar catatan dari basis data atau tempat penyimpanan lainnya
+    # Di sini, hanya menampilkan catatan yang telah disimpan sebelumnya sebagai contoh
+    notes = ["Catatan 1", "Catatan 2", "Catatan 3"]
+    return [html.Li(note) for note in notes]
+  else:
+    return []
+
+
+# Menjalankan aplikasi
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+  app.run_server(debug=True)
